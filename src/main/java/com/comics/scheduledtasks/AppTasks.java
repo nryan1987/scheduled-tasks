c301649c9e8930cd12d04966671fbcd90aa15c64
nryan1987 <nryan.brannan@gmail.com>
2024-08-13 15:49:34 -0500

    Add heart beat

--- src/main/java/com/comics/scheduledtasks/AppTasks.java
+++ src/main/java/com/comics/scheduledtasks/AppTasks.java
@@ -1,27 +1,21 @@
 package com.comics.scheduledtasks;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
+import com.comics.scheduledtasks.util.DateTimeUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.comics.scheduledtasks.application.ApplicationConfiguration;
-import com.comics.scheduledtasks.constants.schedulerConstants;
-
 @Component
 public class AppTasks {
-	@Autowired
-	ApplicationConfiguration config;
-	
-	private static final Logger log = LoggerFactory.getLogger(AppTasks.class);
-	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyy-MM-dd HH:mm:ss");
-	
-	@Scheduled(cron="${heartbeat.cron.schedule}")
-	public void heartBeat() {
-		log.info("The time is now {}", dateFormat.format(new Date()));
-	}
+    @Autowired
+    DateTimeUtil dateTimeUtil;
+
+    private static final Logger logger = LoggerFactory.getLogger(AppTasks.class);
+
+    @Scheduled(cron = "${heartbeat.cron.schedule}")
+    public void heartBeat() {
+        logger.info("The time is now {}", dateTimeUtil.getCurrentCDTTimestamp());
+    }
 }
